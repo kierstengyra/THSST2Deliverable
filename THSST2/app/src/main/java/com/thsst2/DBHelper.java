3bf659eda9288a585c234e5fcecc2264c98da528
kierstengyra <ramoskierstengyra@gmail.com>
2018-01-28 18:27:21 +0800

    [DONE] Start Screen
    - Complete UI (password field and submit button)
    - Functional (connected to database)

--- THSST2/app/src/main/java/com/thsst2/DBHelper.java
+++ THSST2/app/src/main/java/com/thsst2/DBHelper.java
@@ -1,6 +1,8 @@
 package com.thsst2;
 
+//import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -23,18 +25,25 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create tbl_school
+        // Create tbl_school:
         db.execSQL("CREATE TABLE "+TABLE_SCHOOL+
                 "("+COL_SCHOOL_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 +COL_SCHOOL_NAME+" VARCHAR NOT NULL,"+
                 COL_SCHOOL_ADDRESS+" TEXT);");
 
-        // Create tbl_session
+        // Insert values for tbl_school:
+        db.execSQL("INSERT INTO "+TABLE_SCHOOL+"("+COL_SCHOOL_NAME+", "+COL_SCHOOL_ADDRESS+")"+
+                " VALUES ('De La Salle University - Laguna Campus', 'Brgy. Malamig, Binan, Laguna')," +
+                "('Inchican Elementary School', 'Brgy. Inchican, Sta. Rosa, Laguna')," +
+                "('Mary Help of Christians College', 'Brgy. Canlubang, Calamba City, Laguna')," +
+                "('Don Bosco Canlubang', 'Brgy. Canlubang, Calamba City, Laguna')," +
+                "('Don Bosco Children''s Center', 'Brgy. Mayapa, Calamba City, Laguna');");
+
+        // Create tbl_session:
         db.execSQL("CREATE TABLE "+TABLE_SESSION+
                 "("+COL_SESSION_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+
                 COL_SESSION_PASSWORD+" VARCHAR NOT NULL,"+
@@ -42,6 +51,13 @@ public class DBHelper extends SQLiteOpenHelper {
                 COL_SESSION_IS_EXPIRED+" BOOLEAN NOT NULL,"+
                 COL_SCHOOL_ID+" INTEGER,"+
                 "FOREIGN KEY("+COL_SCHOOL_ID+") REFERENCES "+TABLE_SCHOOL+"("+COL_SCHOOL_ID+"));");
+
+        // Insert values for tbl_session:
+        db.execSQL("INSERT INTO " + TABLE_SESSION + "(" + COL_SESSION_PASSWORD + ", " + COL_SESSION_SCHEDULE + "," +
+                COL_SESSION_IS_EXPIRED + "," + COL_SCHOOL_ID + ")" +
+                " VALUES ('fvM41]0:', '2016-02-29 09:00', 0, 3)," +
+                "('8+kA_7%~', '2014-10-17 07:30', 0, 1)," +
+                "('0v(SVFyG', '2018-01-11 08:15', 0, 5);");
     }
 
     @Override
@@ -50,4 +66,27 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_SCHOOL);
         onCreate(db);
     }
+
+    public Cursor confirmSession(String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("SELECT col_school_name FROM "+TABLE_SCHOOL+" as School, "+TABLE_SESSION+" as Session "+
+                "WHERE School.col_school_id = Session.col_school_id AND Session.col_session_password = '"+password+"'", null);
+
+        return result;
+    }
+
+//    public boolean insertSchoolData(String school_name, String school_address) {
+//        SQLiteDatabase db = this.getWritableDatabase();
+//
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(COL_SCHOOL_NAME, school_name);
+//        contentValues.put(COL_SCHOOL_ADDRESS, school_address);
+//
+//        long result = db.insert(TABLE_SCHOOL, null, contentValues);
+//
+//        if(result == -1)
+//            return false;
+//        else
+//            return true;
+//    }
 }
--- THSST2/app/src/main/java/com/thsst2/WelcomeScreen.java
+++ THSST2/app/src/main/java/com/thsst2/WelcomeScreen.java
@@ -1,12 +1,19 @@
 package com.thsst2;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class WelcomeScreen extends AppCompatActivity {
 
     ImageView imgBackground;
+    Button btnStart;
+    EditText txtPassword;
 
     DBHelper dbHelper;
 
@@ -18,6 +25,24 @@ public class WelcomeScreen extends AppCompatActivity {
         this.imgBackground = (ImageView) findViewById(R.id.imgBackground);
         this.imgBackground.setScaleType(ImageView.ScaleType.FIT_XY);
 
+        this.btnStart = (Button) findViewById(R.id.btnStart);
+        this.txtPassword = (EditText) findViewById(R.id.txtPassword);
+
         this.dbHelper = new DBHelper(this);
+    }
+
+    public void verifyPassword(View view) {
+        Cursor result = this.dbHelper.confirmSession(this.txtPassword.getText().toString());
+        if(result.getCount() == 0) {
+            Toast.makeText(WelcomeScreen.this, "Invalid password.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            StringBuffer buffer = new StringBuffer();
+            while(result.moveToNext()) {
+                buffer.append("School Name: "+result.getString(0));
+            }
+
+            Toast.makeText(WelcomeScreen.this, buffer, Toast.LENGTH_SHORT).show();
+        }
     }
 }
