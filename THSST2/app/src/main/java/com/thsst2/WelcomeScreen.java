573070e73a247354d6bcbdf5b29752eed8b36945
kierstengyra <ramoskierstengyra@gmail.com>
2018-01-28 13:32:40 +0800

    Initial commit
    With start screen.

---
+++ THSST2/app/src/main/java/com/thsst2/WelcomeScreen.java
@@ -0,0 +1,19 @@
+package com.thsst2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    ImageView imgBackground;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        this.imgBackground = (ImageView) findViewById(R.id.imgBackground);
+        this.imgBackground.setScaleType(ImageView.ScaleType.FIT_XY);
+    }
+}
