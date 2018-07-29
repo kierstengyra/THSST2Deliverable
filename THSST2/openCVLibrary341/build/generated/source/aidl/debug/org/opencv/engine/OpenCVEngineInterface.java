130769a32bfe7624a084aede69beed7b914be6e7
kierstengyra <ramoskierstengyra@gmail.com>
2018-07-30 01:21:45 +0800

    TODO: Summary and confirmation of answers.

--- THSST2/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ THSST2/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\gyra\\Documents\\GitHub\\THESISFinalVer\\THSST2Deliverable\\THSST2\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\gyra\\Documents\\Thesis\\Deliverables\\THSST-3\\THSST3Deliverable\\THSST2\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
